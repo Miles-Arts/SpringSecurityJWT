ea889f3778c29a396bb452718407f807a9766035
Milton Figueredo Miles <gratno@gmail.com>
2024-03-12 12:40:15 -0500

    Create anotations UserEntity

--- src/main/java/com/example/models/UserEntity.java
+++ src/main/java/com/example/models/UserEntity.java
@@ -1,11 +1,38 @@
 package com.example.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+
+    @Email
+    @NotBlank
+    @Size(max = 80)
     private String email;
+
+    @NotBlank
+    @Size(max = 30)
     private String userName;
+
+    @NotBlank
     private String password;
 
 }
